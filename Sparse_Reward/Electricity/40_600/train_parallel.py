13915a6520ed144481cbe278f51ecf4e734d38d5
Frensi Zejnullahu <frensi@flair-node-12.ddns.eng.ox.ac.uk>
2025-05-15 15:31:27 +0100

    update

--- Sparse_Reward/Electricity/40_600/train_parallel.py
+++ Sparse_Reward/Electricity/40_600/train_parallel.py
@@ -26,7 +26,7 @@ os.makedirs(MODELS_DIR, exist_ok=True)
 # Settings for hyperparameter search
 PARALLEL_CONFIG = {
     
-    'num_seeds': 6,
+    'num_seeds': 30,
     
     'param_grid': {
 
@@ -51,28 +51,26 @@ PARALLEL_CONFIG = {
         'd_epochs': [10],
 
         # PPO parameters
-        #'ppo_total_timesteps': [30 * 6],
-        'ppo_total_timesteps': [40 * 80],
-        'ppo_n_steps': [40 * 2],
-        'ppo_batch_size': [40 * 1],
-        'ppo_n_epochs': [2],
+        'ppo_total_timesteps': [40 * 50],
+        'ppo_n_steps': [40 * 1],
+        'ppo_batch_size': [40 // 2],
+        'ppo_n_epochs': [1],
 
         'use_linear_lr_decay': [False],
         'min_ppo_lr': [1e-5],
 
-        'ppo_learning_rate': [2e-5, 1e-5, 9e-6, 8e-6, 7e-6, 6e-6, 5e-6, 4e-6, 3e-6, 2e-6, 1e-6, 9e-7],
-        #'ppo_learning_rate': [9e-6],
+        'ppo_learning_rate': [1e-6],
         
-        'ppo_gamma': [0.999],
+        'ppo_gamma': [0.9999],
         'ppo_gae_lambda': [0.95],
-        'ppo_clip_range': [0.1],
-        'ppo_ent_coef': [0.001],
-        'ppo_vf_coef': [0.75],
+        'ppo_clip_range': [0.2],
+        'ppo_ent_coef': [0.0],
+        'ppo_vf_coef': [0.5],
         'ppo_clip_range_vf': [None],
         'ppo_max_grad_norm': [0.5],
         
         'do_pretrain': [False],
-        'do_hyperparam_search': [True],
+        'do_hyperparam_search': [False],
         
         # Weight transfer
         'transfer_weights': [True],
@@ -88,7 +86,7 @@ def get_config_hash(config):
 
 def get_free_gpus():
     """Find all free GPUs to use from the allowed GPUs."""
-    allowed_gpus = [0,3,4,5,6,7]  # Only use these GPUs
+    allowed_gpus = [0,1,2,3,4,5,6,7]  # Only use these GPUs
     try:
         result = subprocess.run(
             ['nvidia-smi', '--query-gpu=memory.used,memory.free,utilization.gpu', '--format=csv,nounits,noheader'], 
